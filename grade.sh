105315513f8468485d5dace9f6e15eb1c9a5341a
dylantrann <dylantran3037@gmail.com>
2023-02-15 14:34:43 -0800

    yay
    yay

--- grade.sh
+++ grade.sh
@@ -1,5 +1,49 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar'
 
+
+set -e 
+
+#Step 1: Clone student submission
 rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
+cd student-submission
+
+#Step 2: Check if file exists
+if [[ -f ListExamples.java ]]
+then 
+    echo 'ListExamples.java found'
+else 
+    echo 'ListExamples.java not found'
+    exit 1 
+fi 
+
+#Step 3 Get tester into same directory as student file
+mv ListExamples.java ..
+
+#Step 4: Compile test and student file
+set +e
+cd ..
+
+javac -cp $CPATH ListExamples.java TestListExamples.java 2> compile-output.txt
+
+if [[ $? -eq 0 ]]
+then
+    echo "Compile successful"
+else
+    echo "Compile failed"
+    cat compile-output.txt
+    exit $?
+fi
+
+#Step 5: Run the code
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > run-output.txt 
+
+grepCount=`grep -c "testMergeRightEnd" run-output.txt`
+
+if [[ $grepCount -eq 0 ]]
+then
+    echo "Test passed"
+else
+    echo "Test failed"
+fi
